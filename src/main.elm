62c2ca8bd1b8962728af313a09d220b821906da5
netpro2k <dominick@altvr.com>
2016-06-20 01:52:45 -0700

    Mostly cleanup

--- src/main.elm
+++ src/main.elm
@@ -7,10 +7,10 @@ import Html.Events exposing (..)
 
 import String as String
 import Array
-import Dict
+import Debug
 
 main =
-  Html.beginnerProgram { model = model, view = view, update = update }
+  Html.beginnerProgram { model = defaultModel, view = view, update = update }
 
 
 type Color = Red | Green | Blue | Black
@@ -21,9 +21,10 @@ type alias Token = {
   color:Color,
   dynasty:Dynasty
 }
-
+type alias TileId = Int
 type alias Tile = {
-  color: Color
+  color: Color,
+  id : TileId
 }
 type alias Space = {
   spaceType:SpaceType,
@@ -31,31 +32,37 @@ type alias Space = {
   token:Maybe Token
 }
 type alias Board = Array.Array Space
-type alias Hand = Array.Array Tile
+type alias Hand = List Tile
 type alias Player = {
   dynasty : Dynasty,
   hand : Hand,
-  selectedTile : Maybe Int
+  selectedTile : Maybe Tile
 }
 
 -- Model
 
 type alias Model = {
+  bag : List Tile,
   board : Board,
-  players : Array.Array Player,
-  activePlayer : Int
+  players : List Player
 }
 
-model : Model
-model =
-  {
-    board = startingBoard,
-    players = Array.fromList [
-      (Player Archer (Array.fromList (List.map Tile [Red, Blue, Green, Black, Red, Green])) Nothing),
-      (Player Bull (Array.fromList (List.map Tile [Green, Blue, Blue, Black, Red, Red])) Nothing)
-    ],
-    activePlayer = 0
-  }
+defaultModel : Model
+defaultModel =
+  let
+    redTiles = List.map (Tile Red) [0..57]
+    greenTiles = List.map (Tile Green) [0..30]
+    blackTiles = List.map (Tile Black) [0..30]
+    blueTiles = List.map (Tile Blue) [0..36]
+  in
+    {
+      bag = List.concat [redTiles, greenTiles, blackTiles, blueTiles],
+      board = startingBoard,
+      players = [
+        (Player Archer [] Nothing),
+        (Player Bull [] Nothing)
+      ]
+    }
 
 startingBoard : Board
 startingBoard =
@@ -82,71 +89,83 @@ startingBoard =
 
 -- Update
 
-type Msg = NewGame | PlaceTile Int | SelectTile Player (Maybe Int)
+type Msg = NewGame | PlaceTile Int | SelectTile Player (Maybe Tile) | DrawTile Tile | EndTurn
+
+getActivePlayer : Model -> Maybe Player
+getActivePlayer model =
+  (List.head model.players)
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
     NewGame ->
       model
-    PlaceTile loc ->
+
+    EndTurn ->
+      model
+
+    DrawTile tile ->
       let
+        (drawnTiles, bag') =
+          List.partition ((==) tile) model.bag
         activePlayer =
-          (Array.get model.activePlayer model.players)
-        selectedTileIndex =
-          activePlayer `Maybe.andThen` (\player -> player.selectedTile)
-        selectedTile =
-          case (activePlayer, selectedTileIndex) of
-            (Just player, Just tileIndex) ->
-              Array.get tileIndex player.hand
-            (_, _) ->
-              Nothing
+          getActivePlayer model
         updatePlayer i player =
-          if i == model.activePlayer then
-            case player.selectedTile of
-              Just tileIndex ->
-                { player | selectedTile = Nothing,
-                           hand = (arrayRemove tileIndex player.hand) }
-              Nothing  -> player
+          if i == 0 then
+            { player | hand = (List.append player.hand drawnTiles) }
           else
             player
-
+        players' =
+          List.indexedMap updatePlayer model.players
       in
-        if isValidMove model.board loc selectedTile then
-          { model | board = (placeTile model.board loc selectedTile),
-                    players = (Array.indexedMap updatePlayer model.players) }
-        else
+        { model | players = players', bag = bag' }
+
+    PlaceTile loc ->
+      case ((getActivePlayer model), (getActivePlayer model) `Maybe.andThen` .selectedTile) of
+        (Just activePlayer, Just selectedTile) ->
+          let
+            hand' =
+              List.filter ((/=) selectedTile) activePlayer.hand
+            updatePlayer player =
+              if player == activePlayer then
+                { player | selectedTile = Nothing, hand = hand' }
+              else
+                player
+          in
+            if isValidMove model.board loc selectedTile then
+              { model | board = (placeTile model.board loc selectedTile),
+                        players = (List.map updatePlayer model.players) }
+            else
+              model
+        (_, _) ->
           model
 
-    SelectTile player tileIndex ->
+    SelectTile player tile ->
       let
         selectTiles p =
           if p == player then
-            { p | selectedTile = tileIndex }
+            { p | selectedTile = tile }
           else
             p
       in
-        { model | players = Array.map selectTiles model.players }
+        { model | players = List.map selectTiles model.players }
 
-arrayRemove : Int -> Array.Array a -> Array.Array a
-arrayRemove index array =
-  Array.append (Array.slice 0 index array) (Array.slice (index + 1) (Array.length array) array)
-
-placeTile : Board -> Int -> Maybe Tile -> Board
+placeTile : Board -> Int -> Tile -> Board
 placeTile board loc newTile =
   case (Array.get loc board) of
     Just space ->
-      Array.set loc { space | tile = newTile } board
+      Array.set loc { space | tile = Just newTile } board
     Nothing -> board
 
-isValidMove : Board -> Int -> Maybe Tile -> Bool
+isValidMove : Board -> Int -> Tile -> Bool
 isValidMove board loc tile =
-  case ((Array.get loc board), tile) of
-    (Just space, Just newTile) ->
-      case space.spaceType of
-        Regular -> newTile.color /= Blue
-        Water -> newTile.color == Blue
-    (_, _) -> False
+  case (Array.get loc board) of
+    Just space ->
+      case (space.tile, space.spaceType) of
+        (Just tile, _) -> False
+        (_, Regular) -> tile.color /= Blue
+        (_, Water) -> tile.color == Blue
+    Nothing -> False
 
 
 
@@ -161,18 +180,19 @@ renderTile tile =
       ((String.toLower <| toString <| tile.color), True)
     ]
   in
-    div [classList tileClass] []
+    div [classList tileClass] [text <| toString <| tile.id]
 
 renderSpace : Int -> Space -> Html Msg
 renderSpace i s =
   let
-    spaceClass = ("space " ++ (String.toLower <| toString <| s.spaceType))
+    spaceClass = [("space", True),
+                  (String.toLower <| toString <| s.spaceType, True)]
     tile =
       case s.tile of
         Just t -> [renderTile t]
         Nothing -> []
   in
-    div [(class spaceClass),
+    div [(classList spaceClass),
          onClick (PlaceTile i)] tile
 
 renderPlayer : Player -> Html Msg
@@ -182,9 +202,9 @@ renderPlayer player =
       Just t -> toString t
       Nothing -> "Nothing Selected"
     renderHand hand =
-      div [class "hand"] (Array.toList (Array.indexedMap renderHandSpace hand))
-    renderHandSpace i tile =
-      div [(classList [("space", True), ("selected", player.selectedTile == Just i)]), (onClick (SelectTile player (Just i)))] [
+      div [class "hand"] (List.map renderHandSpace hand)
+    renderHandSpace tile =
+      div [(classList [("space", True), ("selected", player.selectedTile == Just tile)]), (onClick (SelectTile player (Just tile)))] [
         renderTile tile
       ]
   in
@@ -196,12 +216,24 @@ renderPlayer player =
       ]
     ]
 
-view : Model -> Html Msg
-view model =
+renderBag : List Tile -> Html Msg
+renderBag tiles =
   let
-    board = model.board
+    renderBagSpace tile =
+        div [class "space", onClick (DrawTile tile)] [
+          renderTile tile
+        ]
   in
     div [] [
-      div [class "board"] (Array.toList <| Array.indexedMap renderSpace board),
-      div [] (Array.toList (Array.map renderPlayer model.players))
+      (h1 [] [text ("Bag " ++ (toString <| List.length <| tiles))]),
+      div [class "bag"] (List.map renderBagSpace tiles)
     ]
+
+view : Model -> Html Msg
+view model =
+  div [] [
+    div [class "board"] (Array.toList <| Array.indexedMap renderSpace model.board),
+    button [onClick EndTurn] [text "End Turn"],
+    div [] (List.map renderPlayer model.players),
+    renderBag model.bag
+  ]
